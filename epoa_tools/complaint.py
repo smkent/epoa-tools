f3450384e9a19962cdc5bbe1b5133b434edf1a17
Stephen Kent <smkent@smkent.net>
2023-01-26 21:24:44 -0800

    Add form template

--- epoa_tools/complaint.py
+++ epoa_tools/complaint.py
@@ -1,2 +1,29 @@
+from functools import cached_property
+from importlib import resources
+from pathlib import Path
+from typing import Generator
+
+FORM_FILE = "F700-200-000.pdf"
+
+
+class Builder:
+    @cached_property
+    def form_path(self) -> Path:
+        print("GET PATH")
+        return next(self._get_form_path())
+
+    def _get_form_path(self) -> Generator[Path, None, None]:
+        package = "epoa_tools.data"
+        try:
+            with resources.as_file(  # type: ignore
+                resources.files(package).joinpath(FORM_FILE)  # type: ignore
+            ) as path:
+                yield path
+        except AttributeError:
+            with resources.path(package, FORM_FILE) as path:
+                yield path
+
+
 def main() -> None:
-    print("hello")
+    b = Builder()
+    print(b.form_path)
